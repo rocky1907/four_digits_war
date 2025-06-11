959494f7a9555c950b0e3c75eb007b3a2202862d
ralemanU <raleman@DESKTOP-7NSIR79.localdomain>
2025-06-11 09:07:34 -0600

    update settings

--- four_digits_war_backend/four_digits_war_backend/settings.py
+++ four_digits_war_backend/four_digits_war_backend/settings.py
@@ -17,8 +17,12 @@ import logging
 import logging.config
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+LOG_DIR = BASE_DIR / 'logs'
+LOG_DIR.mkdir(parents=True, exist_ok=True)
 
 env = environ.Env(
     DEBUG=(bool, False),
